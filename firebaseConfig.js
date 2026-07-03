ee66f86df9c92629544b75ba1472aaa54bf77c84
eliasroberto26-arch <eliasroberto26@gmail.com>
2026-07-03 09:06:47 -0300

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,9 @@
-// firebase.js - ARQUIVO COMPLETO DO MEETPERTO
+// firebaseConfig.js - PROJETO MEETPERTO2112
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Configuração do seu app MeetPerto
 const firebaseConfig = {
   apiKey: "AIzaSyAFUucTYq7ZO0ki2UO83vtOvcjLgPEMp9A",
   authDomain: "meetperto2112.firebaseapp.com",
@@ -14,10 +13,8 @@ const firebaseConfig = {
   appId: "1:525891276454:web:4d81481caa246b668b2e67"
 };
 
-// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exporta tudo que você vai usar no app
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
